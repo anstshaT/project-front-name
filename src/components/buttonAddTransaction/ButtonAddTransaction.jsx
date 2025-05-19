c9b8924e729de96c704e247103f088e615cc786f
Olena <alchimeraa@gmail.com>
2025-05-19 11:28:07 +0300

    зроблено modal та buttonAddTransaction

---
+++ src/components/buttonAddTransaction/ButtonAddTransaction.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import ModalAddTransaction from '../modalAddTransaction/ModalAddTransaction.jsx';
+import styles from './ButtonAddTransaction.module.css';
+
+const ButtonAddTransaction = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  return (
+    <>
+      <button className={styles.floatingButton} onClick={openModal}>
+        +
+      </button>
+      {isModalOpen && <ModalAddTransaction onClose={closeModal} />}
+    </>
+  );
+};
+
+export default ButtonAddTransaction;
