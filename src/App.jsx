332f1b87ff50a21571a3caea36863925b47f94d0
Anastasia_S <sivokonanastasia1@gmail.com>
2025-05-21 13:43:51 +0300

    transaction type  taggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,41 @@
+import { Suspense, useEffect } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
-import { Register } from "./pages/Register";
+import HomePage from "./pages/HomePage/HomePage";
+import { Register } from "./pages/RegisterPage/Register";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import { Toaster } from "react-hot-toast";
+import StatisticsPage from "./pages/StatisticsPage/StatisticsPage";
+import CurrencyPage from "./pages/CurrencyPage/CurrencyPage";
+import PrivateRoute from "./PrivateRoute";
+import { useDispatch, useSelector } from "react-redux";
+import { selectIsRefreshing } from "./redux/auth/selectors";
+import { refreshUser } from "./redux/auth/authOperations";
 
 function App() {
+  // const isRefreshing = useSelector(selectIsRefreshing);
+  // const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   dispatch(refreshUser());
+  // }, [dispatch]);
+
+  // if (isRefreshing) return null;
   return (
     <>
       <Suspense fallback={<p>Loading...</p>}>
         <Routes>
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<Register />} />
           <Route element={<PrivateRoute />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/statistic" element={<Statistic />} />
-            <Route path="/currency" element={<Currency />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/statistic" element={<StatisticsPage />} />
+            <Route path="/currency" element={<CurrencyPage />} />
           </Route>
-
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Suspense>
+      <Toaster position="top-center" reverseOrder={false} />
     </>
   );
 }
