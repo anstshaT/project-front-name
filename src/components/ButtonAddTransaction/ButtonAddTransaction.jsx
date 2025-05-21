1fee9cc43ddd2e4b70e4232e2e7210fd6eba1aed
Olena <alchimeraa@gmail.com>
2025-05-22 00:00:23 +0300

    Перейменування папкі

--- src/components/ButtonAddTransaction/ButtonAddTransaction.jsx
+++ src/components/ButtonAddTransaction/ButtonAddTransaction.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import ModalAddTransaction from '../modalAddTransaction/ModalAddTransaction.jsx';
+import ModalAddTransaction from '../ModalAddTransaction/ModalAddTransaction.jsx';
 import styles from './ButtonAddTransaction.module.css';
 
 const ButtonAddTransaction = () => {
