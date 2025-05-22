c5eb54c7f94431748840b7a4d41c610752f06be6
Anastasiia Tabachnikova <nastiatab11@gmail.com>
2025-05-22 20:25:35 +0200

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,9 @@
-import AddTransactionForm from "../../components/AddTransactionForm/AddTransactionForm";
 import Header from "../../components/Header/Header";
 
 const HomePage = () => {
   return (
     <div>
       <Header />
-      <AddTransactionForm />
     </div>
   );
 };
