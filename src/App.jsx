4d34a1c04b21a3d762115986479dc1d588d4a12c
MerezkaYurii <shkolasad@ukr.net>
2025-05-22 08:59:43 +0300

    var5

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,22 @@ import { Register } from "./pages/RegisterPage/Register";
 
 function App() {
   return (
-    <>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route element={<PrivateRoute />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/statistic" element={<Statistic />} />
-            <Route path="/currency" element={<Currency />} />
-          </Route>
+    <Register />
+    // <>
+    //   <Suspense fallback={<p>Loading...</p>}>
+    //     <Routes>
+    //       <Route path="/login" element={<Login />} />
+    //       <Route path="/register" element={<Register />} />
+    //       <Route element={<PrivateRoute />}>
+    //         <Route path="/" element={<Home />} />
+    //         <Route path="/statistic" element={<Statistic />} />
+    //         <Route path="/currency" element={<Currency />} />
+    //       </Route>
 
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </Suspense>
-    </>
+    //       <Route path="*" element={<Navigate to="/" />} />
+    //     </Routes>
+    //   </Suspense>git
+    // </>
   );
 }
 
