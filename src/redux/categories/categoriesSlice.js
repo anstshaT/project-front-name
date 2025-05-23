e057c767473752ad416711f8ca649a89b386f0fe
Anastasiia Tabachnikova <nastiatab11@gmail.com>
2025-05-24 01:38:44 +0200

    Add Formik and categories from backend

---
+++ src/redux/categories/categoriesSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { fetchCategories } from "./categoriesOperation";
+
+const categoriesSlice = createSlice({
+  name: "categories",
+  initialState: {
+    categories: [],
+    isLoading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCategories.pending, (state) => {
+        state.isLoading = true;
+        state.error = false;
+      })
+      .addCase(fetchCategories.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.categories = action.payload;
+      })
+      .addCase(fetchCategories.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export default categoriesSlice.reducer;
