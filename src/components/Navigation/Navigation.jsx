0eb7a12cfc9589f975d217982c3b0e379859f5d5
Anastasiia Tabachnikova <nastiatab11@gmail.com>
2025-05-24 23:01:41 +0200

    Add changes

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
-import css from './Navigation.module.css';
+import { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import css from "./Navigation.module.css";
 
 export const Navigation = () => {
   const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
@@ -10,30 +10,45 @@ export const Navigation = () => {
       setIsMobile(window.innerWidth < 768);
     };
 
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return (
     <nav className={css.nav}>
-      <NavLink to="/" className={({ isActive }) => `${css.link} ${isActive ? css.active : ''}`}>
+      <NavLink
+        to="/"
+        className={({ isActive }) =>
+          `${css.link} ${isActive ? css.active : ""}`
+        }
+      >
         <svg className={css.icon} width="24" height="24">
-          <use href="/icons.svg#baseline-home-24px" />
+          <use href="/icons.svg#icon-home1" />
         </svg>
         <span className={css.label}>Home</span>
       </NavLink>
 
-      <NavLink to="/statistics" className={({ isActive }) => `${css.link} ${isActive ? css.active : ''}`}>
+      <NavLink
+        to="/statistics"
+        className={({ isActive }) =>
+          `${css.link} ${isActive ? css.active : ""}`
+        }
+      >
         <svg className={css.icon} width="24" height="24">
-          <use href="/icons.svg#baseline-timeline-24px" />
+          <use href="/icons.svg#icon-statistics" />
         </svg>
         <span className={css.label}>Statistics</span>
       </NavLink>
 
       {isMobile && (
-        <NavLink to="/currency" className={({ isActive }) => `${css.link} ${isActive ? css.active : ''}`}>
-          <svg className={css.icon} width="24" height="24">
-            <use href="/icons.svg#baseline-currency-24px" />
+        <NavLink
+          to="/currency"
+          className={({ isActive }) =>
+            `${css.linkCurrency} ${isActive ? css.activeCurrency : ""}`
+          }
+        >
+          <svg className={css.iconCurrency} width="24" height="24">
+            <use href="/icons.svg#icon-currency" />
           </svg>
           <span className={css.label}>Currency</span>
         </NavLink>
