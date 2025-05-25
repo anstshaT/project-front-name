b101fad9e8a800756cca99cf6c8477f57017422e
Anastasiia Tabachnikova <nastiatab11@gmail.com>
2025-05-25 23:11:12 +0200

    Fixx userLayout

--- src/pages/UserLayout/UserLayout.jsx
+++ src/pages/UserLayout/UserLayout.jsx
@@ -4,16 +4,21 @@ import { Outlet } from "react-router-dom";
 import Header from "../../components/Header/Header";
 import LogoutModal from "../../components/LogoutModal/LogoutModal";
 import ModalAddTransaction from "../../components/ModalAddTransaction/ModalAddTransaction";
+import s from "./UserLayout.module.css";
 
 const UserLayout = () => {
   return (
     <>
       <Header />
-      <main>
-        <SideBar />
-        <Suspense fallback={<p>Loading...</p>}>
-          <Outlet />
-        </Suspense>
+      <main className={s.main}>
+        <div className={s.userLayout}>
+          <div className={s.sideBar}>
+            <SideBar />
+          </div>
+          <Suspense fallback={<p>Loading...</p>}>
+            <Outlet />
+          </Suspense>
+        </div>
         {/* <LogoutModal />
         <ModalAddTransaction /> */}
         {/* <ModalEditTransaction /> */}
