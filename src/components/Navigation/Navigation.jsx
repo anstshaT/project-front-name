873990e08d7e0824786aae0125dd38a3bdb59e26
Olena <alchimeraa@gmail.com>
2025-05-25 19:38:47 +0300

    стилі Navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -7,65 +7,40 @@ const Navigation = () => {
   const isMobile = useMediaQuery({ maxWidth: 767 });
 
   return (
-    <nav className={css.nav}>
-      <NavLink to="/" className={({ isActive }) => isActive ? css.linkActive : css.link}>
-        <svg className={css.icon}>
-          <use href="/icons.svg#baseline-home-24px" />
-        </svg>
-        <span className={css.label}>Home</span>
-      </NavLink>
-
-      <NavLink to="/statistics" className={({ isActive }) => isActive ? css.linkActive : css.link}>
-        <svg className={css.icon}>
-          <use href="/icons.svg#icon-statistics" />
-        </svg>
-        <span className={css.label}>Statistics</span>
-      </NavLink>
-
-      {isMobile && (
-        <NavLink to="/currency" className={({ isActive }) => isActive ? css.linkActive : css.link}>
-          <svg className={css.icon}>
-            <use href="/icons.svg#icon-currency" />
-          </svg>
+    <div className={css.container}>
+      <nav className={css.nav}>
+        <NavLink to="/" className={({ isActive }) => `${css.linkContainer} ${isActive ? css.linkActive : ''}`}>
+          <div className={css.iconWrapper}>
+            <svg className={css.icon}>
+              <use href="/icons.svg#icon-home1" />
+            </svg>
+          </div>
+          {!isMobile && <span className={css.label}>Home</span>}
         </NavLink>
-      )}
-    </nav>
+
+        <NavLink to="/statistics" className={({ isActive }) => `${css.linkContainer} ${isActive ? css.linkActive : ''}`}>
+          <div className={css.iconWrapper}>
+            <svg className={css.icon}>
+              <use href="/icons.svg#icon-statistics" />
+            </svg>
+          </div>
+          {!isMobile && <span className={css.label}>Statistics</span>}
+        </NavLink>
+
+        {isMobile && (
+          <NavLink to="/currency" className={({ isActive }) =>
+            `${css.linkContainer} ${css.currency} ${isActive ? css.linkActive : ''}`
+          }>
+            <div className={css.iconWrapper}>
+              <svg className={css.icon}>
+                <use href="/icons.svg#icon-currency" />
+              </svg>
+            </div>
+          </NavLink>
+        )}
+      </nav>
+    </div>
   );
 };
 
 export { Navigation };
-
-// import React from 'react';
-// import { NavLink } from 'react-router-dom';
-// import css from './Navigation.module.css';
-
-// const Navigation = () => {
-//   return (
-//     <nav className={css.nav}>
-//       {/* Home */}
-//       <NavLink to="/" className={({ isActive }) => isActive ? css.linkActive : css.link}>
-//         <svg className={css.icon}>
-//           <use href="/icons.svg#baseline-home-24px" />
-//         </svg>
-//         <span className={css.label}>Home</span>
-//       </NavLink>
-
-//       {/* Statistics */}
-//       <NavLink to="/statistics" className={({ isActive }) => isActive ? css.linkActive : css.link}>
-//         <svg className={css.icon}>
-//           <use href="/icons.svg#baseline-timeline-24px" />
-//         </svg>
-//         <span className={css.label}>Statistics</span>
-//       </NavLink>
-
-//       {/* Currency (для мобілки тільки) */}
-//       <NavLink to="/currency" className={({ isActive }) => isActive ? css.linkActive : css.link} id={css.currencyLink}>
-//         <svg className={css.icon}>
-//           <use href="/icons.svg#icon-currency" />
-//         </svg>
-//       </NavLink>
-//     </nav>
-//   );
-// };
-
-// export { Navigation };
