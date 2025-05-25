c49be608468ea4fc3e38daab9fd36bdd752b6964
konyaev <konyaevd1995@gmail.com>
2025-05-26 00:34:37 +0300

    add stat-dashboard

---
+++ src/pages/StatisticsPage/StatisticsPage.jsx
@@ -0,0 +1,15 @@
+import TransactionType from "../../components/TransactionType/TransactionType";
+
+const StatisticsPage = () => {
+  return (
+    <div>
+      <h1>Statistic</h1>
+      <TransactionType />
+      {/* <Chart />
+      <StatisticsDashboard />
+      <StatisticsTable /> */}
+    </div>
+  );
+};
+
+export default StatisticsPage;
