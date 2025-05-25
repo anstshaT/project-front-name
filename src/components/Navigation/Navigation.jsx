bc58b7d6d8fc3b0dd65fb1ca7acae95597ced51f
Olena <alchimeraa@gmail.com>
2025-05-25 15:26:36 +0300

    стилі Navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import css from './Navigation.module.css';
+import { useMediaQuery } from 'react-responsive';
 
 const Navigation = () => {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
   return (
     <nav className={css.nav}>
-      {/* Home */}
       <NavLink to="/" className={({ isActive }) => isActive ? css.linkActive : css.link}>
         <svg className={css.icon}>
           <use href="/icons.svg#baseline-home-24px" />
@@ -13,22 +15,57 @@ const Navigation = () => {
         <span className={css.label}>Home</span>
       </NavLink>
 
-      {/* Statistics */}
       <NavLink to="/statistics" className={({ isActive }) => isActive ? css.linkActive : css.link}>
         <svg className={css.icon}>
-          <use href="/icons.svg#baseline-timeline-24px" />
+          <use href="/icons.svg#icon-statistics" />
         </svg>
         <span className={css.label}>Statistics</span>
       </NavLink>
 
-      {/* Currency (для мобілки тільки) */}
-      <NavLink to="/currency" className={({ isActive }) => isActive ? css.linkActive : css.link} id={css.currencyLink}>
-        <svg className={css.icon}>
-          <use href="/icons.svg#icon-currency" />
-        </svg>
-      </NavLink>
+      {isMobile && (
+        <NavLink to="/currency" className={({ isActive }) => isActive ? css.linkActive : css.link}>
+          <svg className={css.icon}>
+            <use href="/icons.svg#icon-currency" />
+          </svg>
+        </NavLink>
+      )}
     </nav>
   );
 };
 
 export { Navigation };
+
+// import React from 'react';
+// import { NavLink } from 'react-router-dom';
+// import css from './Navigation.module.css';
+
+// const Navigation = () => {
+//   return (
+//     <nav className={css.nav}>
+//       {/* Home */}
+//       <NavLink to="/" className={({ isActive }) => isActive ? css.linkActive : css.link}>
+//         <svg className={css.icon}>
+//           <use href="/icons.svg#baseline-home-24px" />
+//         </svg>
+//         <span className={css.label}>Home</span>
+//       </NavLink>
+
+//       {/* Statistics */}
+//       <NavLink to="/statistics" className={({ isActive }) => isActive ? css.linkActive : css.link}>
+//         <svg className={css.icon}>
+//           <use href="/icons.svg#baseline-timeline-24px" />
+//         </svg>
+//         <span className={css.label}>Statistics</span>
+//       </NavLink>
+
+//       {/* Currency (для мобілки тільки) */}
+//       <NavLink to="/currency" className={({ isActive }) => isActive ? css.linkActive : css.link} id={css.currencyLink}>
+//         <svg className={css.icon}>
+//           <use href="/icons.svg#icon-currency" />
+//         </svg>
+//       </NavLink>
+//     </nav>
+//   );
+// };
+
+// export { Navigation };
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import CurrencyPage from "../../pages/CurrencyPage/CurrencyPage";
 import { Navigation } from "../Navigation/Navigation";
 import css from './SideBar.module.css';
-import { useMediaQuery } from 'react-responsive';
+// import { useMediaQuery } from 'react-responsive';
 import Balance from "../Balance/Balance";
 
 const SideBar = () => {
-  const isMobile = useMediaQuery({ maxWidth: 767 });
-  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1279 });
-  const isDesktop = useMediaQuery({ minWidth: 1280 });
+  // const isMobile = useMediaQuery({ maxWidth: 767 });
+  // const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1279 });
+  // const isDesktop = useMediaQuery({ minWidth: 1280 });
 
     return (
-      <div {/* className={css.sidebarDesktop} */}>
+      <div>
           <Navigation />
           <Balance />
           <CurrencyPage />
