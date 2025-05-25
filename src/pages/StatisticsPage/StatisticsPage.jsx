aa806c35c57390962d8bfb298945a370ef1bd646
Anastasiia Tabachnikova <nastiatab11@gmail.com>
2025-05-25 23:56:39 +0200

    Add changes

--- src/pages/StatisticsPage/StatisticsPage.jsx
+++ src/pages/StatisticsPage/StatisticsPage.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
+import TransactionType from "../../components/TransactionType/TransactionType";
+import Chart from "../../components/Chart/Chart";
 
 const StatisticsPage = () => {
   const [transactionType, setTransactionType] = useState("expense");
   return (
     <div>
-      <TransactionType 
+      <TransactionType
         transactionType={transactionType}
-        setTransactionType={setTransactionType}/>
-      <Chart transactionType={transactionType}/>
+        setTransactionType={setTransactionType}
+      />
+      <Chart transactionType={transactionType} />
     </div>
   );
 };
