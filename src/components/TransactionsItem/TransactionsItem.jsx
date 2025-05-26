6f58b494604e23c9be094755d3ccfa62230a231e
Anastasiia Tabachnikova <nastiatab11@gmail.com>
2025-05-26 13:57:17 +0200

    Fix transactionItem

--- src/components/TransactionsItem/TransactionsItem.jsx
+++ src/components/TransactionsItem/TransactionsItem.jsx
@@ -1,66 +1,76 @@
 import { useDispatch } from "react-redux";
-import css from "./TransactionsItem.module.css"
+import css from "./TransactionsItem.module.css";
 import { MdOutlineEdit } from "react-icons/md";
 import { deleteTransaction } from "../../redux/transactions/transactionsOps";
-import ModalEditTransaction from "../ModalEditTransaction/ModalEditTransaction"
+import ModalEditTransaction from "../ModalEditTransaction/ModalEditTransaction";
 import { useState } from "react";
 
+const TransactionsItem = ({ transaction }) => {
+  const { id, date, type, category, comment, sum } = transaction;
+  const dispatch = useDispatch();
 
-const TransactionsItem = ({transaction}) => {
-     const { id, date, type, category, comment, sum } = transaction;
-     const dispatch = useDispatch();
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-     const [isModalOpen, setIsModalOpen] = useState(false);
+  return (
+    <>
+      <li
+        className={`${css.transactionItem} ${
+          type === "income" ? css.incomeLine : css.expenseLine
+        }`}
+      >
+        <div className={css.field}>
+          <span className={css.label}>Date</span>
+          <span className={css.value}>{date}</span>
+        </div>
 
-     
-    return (
-     <>
-        <li className={`${css.transactionItem} ${
-            type === 'income' ? css.incomeLine : css.expenseLine
-          }`}>
-            <div className={css.field}>
-                <span className={css.label}>Date</span>
-                <span className={css.value}>{date}</span>
-            </div>
+        <div className={css.field}>
+          <span className={css.label}>Type</span>
+          <span className={css.amount}>{type === "income" ? "+" : "-"}</span>
+        </div>
 
-           <div className={css.field}>
-                <span className={css.label}>Type</span>
-                <span className={css.amount}>
-                {type === 'income' ? '+' : '-'}</span>
-           </div>
+        <div className={css.field}>
+          <span className={css.label}>Category</span>
+          <span className={css.value}>{category}</span>
+        </div>
 
-           <div className={css.field}>
-                <span className={css.label}>Category</span>
-                <span className={css.value}>{category}</span>
-           </div>
+        <div className={css.field}>
+          <span className={css.label}>Comment</span>
+          <span className={css.value}>{comment}</span>
+        </div>
 
-           <div className={css.field}>
-                <span className={css.label}>Comment</span>
-                <span className={css.value}>{comment}</span>
-           </div>
+        <div className={css.field}>
+          <span className={css.label}>Sum</span>
+          <span
+            className={`${css.value} ${
+              type === "income" ? css.income : css.expense
+            }`}
+          >
+            {sum /* .toFixed(2) */}
+          </span>
+        </div>
+        <div className={css.actions}>
+          <button
+            className={css.deleteBtn}
+            onClick={() => dispatch(deleteTransaction(id))}
+          >
+            Delete
+          </button>
 
-           <div className={css.field}>
-                <span className={css.label}>Sum</span>
-                <span className={`${css.value} ${type === 'income' ? css.income : css.expense}`}>{sum.toFixed(2)}</span>
-          </div>
-          <div className={css.actions}>
-       
-          <button className={css.deleteBtn} onClick={() => dispatch(deleteTransaction(id))}>Delete</button>
-       
-          <button className={css.editBtn} onClick={() =>setIsModalOpen(true)}>
-          <MdOutlineEdit style={{ marginRight: '4px' }}/><span className={css.editText}>Edit</span></button>
-       </div>
-        </li>
+          <button className={css.editBtn} onClick={() => setIsModalOpen(true)}>
+            <MdOutlineEdit style={{ marginRight: "4px" }} />
+            <span className={css.editText}>Edit</span>
+          </button>
+        </div>
+      </li>
 
-
-        {isModalOpen && (
-          <ModalEditTransaction
-            transaction={transaction}
-            onClose={() => setIsModalOpen(false)}/>
-        )}
-        </>
-     );
-     };
+      {isModalOpen && (
+        <ModalEditTransaction
+          transaction={transaction}
+          onClose={() => setIsModalOpen(false)}
+        />
+      )}
+    </>
+  );
+};
 
 export default TransactionsItem;
-
