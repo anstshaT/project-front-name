90dbb6763c2141ade371076ab1178118d125bfaa
Olena <alchimeraa@gmail.com>
2025-05-26 20:11:41 +0300

    Navigation05

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -65,3 +65,78 @@ const Navigation = () => {
 };
 
 export { Navigation };
+
+// import React from "react";
+// import { NavLink } from "react-router-dom";
+// import css from "./Navigation.module.css";
+// import { useMediaQuery } from "react-responsive";
+
+// const Navigation = () => {
+//   const isMobile = useMediaQuery({ maxWidth: 767 });
+
+//   return (
+//     <div className={css.container}>
+//       <nav className={css.nav}>
+//         {/* HOME */}
+//         <NavLink
+//           to="/"
+//           className={({ isActive }) =>
+//             isActive ? `${css.link} ${css.linkActive}` : css.link
+//           }
+//         >
+//           <span className={css.iconWrapper}>
+//             {isMobile ? (
+//               <svg className={`${css.icon} ${css.iconHome}`}>
+//                 <use href="/icons.svg#icon-home1" />
+//               </svg>
+//             ) : (
+//               <svg className={css.icon}>
+//                 <use href="/icons.svg#baseline-home-24px" />
+//               </svg>
+//             )}
+//           </span>
+//           <span className={css.label}>Home</span>
+//         </NavLink>
+
+//         {/* STATISTICS */}
+//         <NavLink
+//           to="/statistic"
+//           className={({ isActive }) =>
+//             isActive ? `${css.link} ${css.linkActive}` : css.link
+//           }
+//         >
+//           <span className={css.iconWrapper}>
+//             {isMobile ? (
+//               <svg className={`${css.icon} ${css.iconStatistics}`}>
+//                 <use href="/icons.svg#icon-statistics" />
+//               </svg>
+//             ) : (
+//               <svg className={css.icon}>
+//                 <use href="/icons.svg#baseline-timeline-24px" />
+//               </svg>
+//             )}
+//           </span>
+//           <span className={css.label}>Statistics</span>
+//         </NavLink>
+
+//         {/* CURRENCY — тільки мобільна */}
+//         {isMobile && (
+//           <NavLink
+//             to="/currency"
+//             className={({ isActive }) =>
+//               isActive ? `${css.link} ${css.linkActive}` : css.link
+//             }
+//           >
+//             <span className={css.iconWrapper}>
+//               <svg className={`${css.icon} ${css.iconCurrency}`}>
+//                 <use href="/icons.svg#icon-currency" />
+//               </svg>
+//             </span>
+//           </NavLink>
+//         )}
+//       </nav>
+//     </div>
+//   );
+// };
+
+// export { Navigation };
